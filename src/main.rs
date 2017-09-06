6ebc1ccd6857c13a4a70ace33dea15c54c5cca5b
John Anthony <john@jo.hnanthony.com>
2017-09-07 00:43:25 +0100

    Added random potion generation

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,55 @@ enum CoinType {
 
 #[derive(Debug)]
 enum PotionType {
-    Healing
+    Longevity,
+    Love,
+    Poison,
+    Climbing,
+    Delusion,
+    Diminutiveness,
+    ExtraGrowth,
+    ExtraHealing,
+    FireResist,
+    Flying,
+    GaseousForm,
+    GiantPower,
+    Healing,
+    HeroicAction,
+    Invincibility,
+    Invisibility,
+    Levitation,
+    SuperHeroicAction,
+    SuperSpeed,
+    WaterBreathing,
+}
+
+impl PotionType {
+    fn random() -> PotionType {
+        let mut rng = rand::thread_rng();
+        match rng.gen::<u64>()%20 {
+            0 => PotionType::Longevity,
+            1 => PotionType::Love,
+            2 => PotionType::Poison,
+            3 => PotionType::Climbing,
+            4 => PotionType::Delusion,
+            5 => PotionType::Diminutiveness,
+            6 => PotionType::ExtraGrowth,
+            7 => PotionType::ExtraHealing,
+            8 => PotionType::FireResist,
+            9 => PotionType::Flying,
+            10 => PotionType::GaseousForm,
+            11 => PotionType::GiantPower,
+            12 => PotionType::Healing,
+            13 => PotionType::HeroicAction,
+            14 => PotionType::Invincibility,
+            15 => PotionType::Invisibility,
+            16 => PotionType::Levitation,
+            17 => PotionType::SuperHeroicAction,
+            18 => PotionType::SuperSpeed,
+            19 => PotionType::WaterBreathing,
+            _ => panic!()
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -45,7 +93,7 @@ fn percent_chance(chance: u8) -> bool {
 }
 
 fn main() {
-    let code = 'p';
+    let code = 's';
     let mut treasures = Vec::new();
 
     match code {
@@ -95,6 +143,16 @@ fn main() {
                     treasure: Treasure::Coins(CoinType::Electrum),
                     quantity: dice_roll(1, 4)
                 })
+            }
+        },
+        's' => {
+            if percent_chance(40) {
+                for _ in 0..dice_roll(2, 4) {
+                    treasures.push(TreasureEntry {
+                        treasure: Treasure::Potion(PotionType::random()),
+                        quantity: 1
+                    })
+                }
             }
         },
         'y' => {
